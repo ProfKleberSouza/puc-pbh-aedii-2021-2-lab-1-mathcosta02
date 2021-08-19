7c5a63e43309696c7d98bf7ca169083669aae61b
Matheus Costa <89105275+mathcosta02@users.noreply.github.com>
2021-08-19 20:35:51 +0000

    Exe04 completo

--- exe04/functions.c
+++ exe04/functions.c
@@ -1,7 +1,33 @@
-
+#include <stdio.h>
+#include <ctype.h>
 #include "functions.h"
 
-int somar(int n1, int n2){
+void numero_vogais(char texto[]){
+  int i=0, contA=0, contE=0, contI=0, contO=0, contU=0;
 
-    return n1+n2;
+  while(texto[i] != '\0'){
+    texto[i] = toupper(texto[i]);
+    if(texto[i] == 'A'){
+      contA++;
+    }
+    if(texto[i] == 'E'){
+      contE++;
+    }
+    if(texto[i] == 'I'){
+      contI++;
+    }
+    if(texto[i] == 'O'){
+      contO++;
+    }
+    if(texto[i] == 'U'){
+      contU++;
+    }
+    i++;
+  }
+
+  printf("A = %d\n", contA);
+  printf("E = %d\n", contE);
+  printf("I = %d\n", contI);
+  printf("O = %d\n", contO);
+  printf("U = %d\n", contU);
 }
